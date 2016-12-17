69374b55932d1ec659ea104d49beaa2eead43658
Onwuzulike Emeka <onwuzulikee1@gmail.com>
2016-12-17 13:36:49 +0100

    corrected json dropdown

--- ClientManagement.Web/Scripts/_references.js
+++ ClientManagement.Web/Scripts/_references.js
@@ -10,3 +10,4 @@
 /// <reference path="modernizr-2.8.3.js" />
 /// <reference path="respond.matchmedia.addlistener.min.js" />
 /// <reference path="respond.min.js" />
+/// <reference path="typed.js" />
